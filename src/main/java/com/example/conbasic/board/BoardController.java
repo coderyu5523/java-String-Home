9104ed2e68e51ee27e80e7289578a5fbe1f088e2
coderyu5523 <coderyu5523@gmail.com>
2024-01-30 23:47:38 +0900

    boardãController 추가

--- src/main/java/com/example/conbasic/board/BoardController.java
+++ src/main/java/com/example/conbasic/board/BoardController.java
@@ -1,4 +1,31 @@
-package com.example.conbasic.board;
+package shop.mtcoding.blog.board;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import shop.mtcoding.blog.user.User;
+
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@Controller
 public class BoardController {
+
+    private final HttpSession session;
+
+    @GetMapping({ "/", "/board" })
+    public String index() {
+
+        return "index";
+    }
+
+    @GetMapping("/board/saveForm")
+    public String saveForm() {
+        return "board/saveForm";
+    }
+
+    @GetMapping("/board/1")
+    public String detail() {
+        return "board/detail";
+    }
 }
