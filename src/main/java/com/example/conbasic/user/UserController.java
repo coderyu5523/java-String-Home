63382cd6d6ecc1d1ee769e1b90dc6a1b1cb16fd5
coderyu5523 <coderyu5523@gmail.com>
2024-02-02 23:17:46 +0900

    ㅕuserController 완성

--- src/main/java/com/example/conbasic/user/UserController.java
+++ src/main/java/com/example/conbasic/user/UserController.java
@@ -3,11 +3,27 @@ package com.example.conbasic.user;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
 
+    private final UserRepository userRepository ;
 
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @PostMapping("/join")
+    public String join(UserRequest.JoinDTO requestDTO){
+
+        if(requestDTO.getUsername().length()<3){
+            return "error/400";
+        }
+        userRepository.save(requestDTO);
+
+        return "redirect:/loginForm";
+    }
 
     @GetMapping("/joinForm")
     public String joinForm(){
---
+++ src/main/java/com/example/conbasic/user/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.conbasic.user;
+
+public class UserRepository {
+
+
+
+    public void save(UserRequest.JoinDTO requestDTO) {
+    }
+}
